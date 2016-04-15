3b0078496d79d121da30ea7128b080778b35bc66
AamirSuhail <aamirsuhail95@live.com>
2016-04-15 18:40:32 +0500

    Practice before mids

---
+++ NewProjects/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class practice
+    {
+        public int factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+
+
+        public string[] StringCombinations(string str, int strings)
+        {
+            char[] chAry = str.ToCharArray();
+            string[] strAry=new string[strings];
+            int length2=(chAry.Length-1)*2;
+            int maxLngth = (chAry.Length)-1;
+            
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i != 0 || chAry[i+1]!='\0')
+                {
+                    char tempr = chAry[i];
+                    chAry[i] = chAry[i + 1];
+                    chAry[i + 1] = tempr;
+                }
+                for (int j = 0; j <length2 ; j++)
+                {
+                    if (maxLngth != 1)
+                    {
+                        char temp = chAry[maxLngth];
+                        chAry[maxLngth] = chAry[maxLngth - 1];
+                        chAry[maxLngth - 1] = temp;
+                        maxLngth--;
+                    }
+                    else
+                        maxLngth = (str.Length)-1;
+                    string name = null;
+                    for (int e = 0; e < chAry.Length; e++)
+                    {
+                        if (chAry[e] != '\0')
+                        {
+                            name += chAry[e];
+                        }
+                    }
+                    strAry[j] = name;
+                }
+            }
+
+            return strAry;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            practice pr = new practice();
+            Console.WriteLine( pr.factorial(4));
+            string[] ary=pr.StringCombinations("Aamir",120);
+            
+            foreach (string item in ary)
+            {
+                Console.WriteLine(item);
+            }
+	
+            
+            
+            //string a = "Aamir Suhail";
+            ////to character arry
+            //char[] ch = a.ToCharArray();
+            //char[] nw=new char[15];
+            //int index = 0;
+            //for (int i = 0; i < ch.Length; i++)
+            //{
+            //    if (ch[i] != ' ')
+            //    {
+            //        nw[index] = ch[i];
+            //        index++;
+            //    }
+            //    else
+            //        continue;
+                
+            //}
+            //for (int i = 0; i < nw.Length; i++)
+            //{
+                
+            //}
+            ////Console.WriteLine(nw.ToString());
+
+
+
+            ////picking a substring 
+            //string sub=a.Substring(6, 3);
+            //Console.WriteLine(sub);
+            
+
+            ////triming
+            //string aamir = "aamir abc ddlj";
+            ////char[] ary = { ' ', 's' };
+            //Console.WriteLine(aamir.Trim());
+            //Array.Reverse(ch);
+            //new string(ch);
+            ////string hh = ch.Reverse<char>().ToString();
+            //Console.WriteLine(ch);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NewProjects/StringFunctions/mixPractice/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mixPractice
+{
+    class student
+    {
+        int nn = 4;
+        //int no1,no2;
+        public void returnByRef(ref int no1,ref int no2)
+        {
+            if (no1 > 3)
+            {
+                no1 = no1 + 1;
+                
+            }
+            else
+                no1 = no1 - 1; ;
+        }
+        public static explicit operator int(object obj)
+        {
+           //s
+            
+        }
+        public void ParametrizdArray(params int[] a)
+        {
+            foreach (int item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+       
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            int v1 = 4;
+	        int v2 = 5;
+	        int v3 = 6;
+                
+            
+            student std = new student();
+            std.returnByRef(ref v1,ref v2);
+            Console.WriteLine(v1);
+
+            //int[] AR = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            std.ParametrizdArray(1, 2, 3, 4, 5, 6, 7, 8);
+            
+
+            byte b = 255;
+            unchecked
+            {
+                b++;
+
+            }
+            Console.WriteLine(b);
+
+            
+            Console.ReadKey();
+        }
+    }
+
+    
+}
